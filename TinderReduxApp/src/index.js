1d7c002e402ebf2c794ece1f19eb61dd22c4e557
jeffski13 <jeffski@okstate.edu>
2017-05-05 16:42:37 -0500

    Tinder Redux App setup

---
+++ TinderReduxApp/src/index.js
@@ -0,0 +1,26 @@
+//using es6 modules
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class JApp extends Component{
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+//new es6 syntax which makes a custom tag ("JeffApp") with html inside
+const JeffApp = () => {
+  return <div>YOLO32</div>;
+}
+
+//put an instance of the JApp component into page (in DOM)
+ReactDOM.render(<JApp />,  document.querySelector('.jContainHer'));
