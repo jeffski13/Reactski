8156489371218e18e07d0eabb9333c1b8ed76ab3
Jeff Szcinski II <jeffskiosu@gmail.com>
2024-07-29 08:37:12 -0700

    scrolling for search app

--- zergski/app/src/main/java/ski/jeff/zergski/apps/unitsearch/FavoriteUnitCard.kt
+++ zergski/app/src/main/java/ski/jeff/zergski/apps/unitsearch/FavoriteUnitCard.kt
@@ -1,5 +1,6 @@
 package ski.jeff.zergski.apps.unitsearch
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -23,7 +24,11 @@ import ski.jeff.zergski.ui.theme.ZergskiTheme
 
 @Composable
 fun FavoriteUnitCard(favoriteUnitCardInfo: FavoriteUnitCardInfo, modifier: Modifier = Modifier) {
-    Surface(modifier = modifier, shape = RoundedCornerShape(8.dp)) {
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        color = MaterialTheme.colorScheme.surfaceVariant
+    ) {
         Column(modifier = Modifier.width(255.dp)){
             Row (verticalAlignment = Alignment.CenterVertically){
                Image(
@@ -33,6 +38,7 @@ fun FavoriteUnitCard(favoriteUnitCardInfo: FavoriteUnitCardInfo, modifier: Modif
                    contentScale = ContentScale.Crop
                )
                Text(favoriteUnitCardInfo.name,
+                   color = MaterialTheme.colorScheme.onSurface,
                    modifier = Modifier.padding(horizontal = 16.dp),
                    style = MaterialTheme.typography.titleMedium)
             }
@@ -40,9 +46,17 @@ fun FavoriteUnitCard(favoriteUnitCardInfo: FavoriteUnitCardInfo, modifier: Modif
     }
 }
 
-@Preview
+@Preview (uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
 @Composable
 fun previewFavoriteUnitCard() {
+    ZergskiTheme {
+        FavoriteUnitCard(favoriteUnitCardInfo = FavoriteUnitCardInfoListProvider.PREVIEW_DATA)
+    }
+}
+
+@Preview (uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun previewFavoriteUnitCardDarkMode() {
     ZergskiTheme {
         FavoriteUnitCard(favoriteUnitCardInfo = FavoriteUnitCardInfoListProvider.PREVIEW_DATA)
     }
--- zergski/app/src/main/java/ski/jeff/zergski/apps/unitsearch/UnitSearchApp.kt
+++ zergski/app/src/main/java/ski/jeff/zergski/apps/unitsearch/UnitSearchApp.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,7 +20,7 @@ import ski.jeff.zergski.ui.theme.ZergskiTheme
 @Composable
 fun UnitSearchApp(modifier: Modifier = Modifier) {
     Surface(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = modifier) {
+        Column(modifier = modifier.verticalScroll(rememberScrollState())) {
             Spacer(modifier = Modifier.height(16.dp))
             SearchBar()
             SearchAppSection(titleId = R.string.buildingList_sectionTitle) {
@@ -27,10 +29,19 @@ fun UnitSearchApp(modifier: Modifier = Modifier) {
             SearchAppSection(titleId = R.string.favoriteUnits_sectionTitle) {
                 FavoriteUnitCardsSection()
             }
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
 
+
+@Preview (uiMode = Configuration.UI_MODE_NIGHT_NO, heightDp = 180)
+@Composable
+fun previewUnitSearchAppWithScroll() {
+    ZergskiTheme {
+        UnitSearchApp()
+    }
+}
 
 @Preview (uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
