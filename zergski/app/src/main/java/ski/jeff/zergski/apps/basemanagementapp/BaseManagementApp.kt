5d9b1307c436be9d2b2539078568131e23dc6f4e
Jeff Szcinski II <jeffskiosu@gmail.com>
2024-07-30 10:17:41 -0700

    Number of drones stateless

--- zergski/app/src/main/java/ski/jeff/zergski/apps/basemanagementapp/BaseManagementApp.kt
+++ zergski/app/src/main/java/ski/jeff/zergski/apps/basemanagementapp/BaseManagementApp.kt
@@ -5,15 +5,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ski.jeff.zergski.ui.theme.ZergskiTheme
 
 @Composable
 fun BaseManagementApp(){
+    var dronesHatched = rememberSaveable { mutableStateOf(0) }
+
     ZergskiTheme {
         Surface(color = MaterialTheme.colorScheme.surface, modifier = Modifier.fillMaxSize()) {
-            NumberOfDrones()
+            NumberOfDrones(dronesHatched.value, {dronesHatched.value++},dronesHatched.value < 5, dronesHatched.value > 0)
         }
     }
 }
--- zergski/app/src/main/java/ski/jeff/zergski/apps/basemanagementapp/NumberOfDrones.kt
+++ zergski/app/src/main/java/ski/jeff/zergski/apps/basemanagementapp/NumberOfDrones.kt
@@ -5,21 +5,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun NumberOfDrones(modifier: Modifier = Modifier){
-    var dronesHatched = rememberSaveable { mutableStateOf(0) }
+fun NumberOfDrones(dronesHatched: Int, onSpawnDrone: () -> Unit, isButtonEnabled: Boolean, isDroneCountShowing: Boolean,  modifier: Modifier = Modifier){
     Column {
-        Button(onClick = {dronesHatched.value++}, enabled = (dronesHatched.value < 5)) {
+        Button(onClick = onSpawnDrone, enabled = isButtonEnabled) {
             Text("spawn drone")
         }
-        if(dronesHatched.value > 0) {
-            Text("Drones Created: ${dronesHatched.value}", modifier = modifier.padding(16.dp))
+        if(isDroneCountShowing) {
+            Text("Drones Created: $dronesHatched", modifier = modifier.padding(16.dp))
         }
     }
 }
@@ -27,5 +24,5 @@ fun NumberOfDrones(modifier: Modifier = Modifier){
 @Preview(showBackground = true)
 @Composable
 fun previewNumberOfDrones() {
-    NumberOfDrones()
+    NumberOfDrones(0, {}, false, false)
 }
