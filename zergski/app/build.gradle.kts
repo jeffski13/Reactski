e89de6fa2d8ab93a728a175070ad8081887621da
Jeff Szcinski II <jeffskiosu@gmail.com>
2024-08-01 14:50:13 -0700

    drone counts migrated to viewmodel

--- zergski/app/build.gradle.kts
+++ zergski/app/build.gradle.kts
@@ -58,12 +58,15 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview-android:1.6.8")
     implementation("androidx.compose.material3:material3-window-size-class-android:1.2.1")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:{latest_version}")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.compose.material:material-icons-extended")
 }
--- zergski/app/src/main/java/ski/jeff/zergski/apps/basemanagementapp/BaseManagementApp.kt
+++ zergski/app/src/main/java/ski/jeff/zergski/apps/basemanagementapp/BaseManagementApp.kt
@@ -12,24 +12,42 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import ski.jeff.zergski.ui.theme.ZergskiTheme
 
 @Composable
-fun BaseManagementApp(){
-    val dronesHatched = rememberSaveable { mutableStateOf(0) }
-    val baseTaskList = remember { BaseTaskProvider.getBaseTaskList().toMutableStateList() }
-
-    println("redrawing BaseManagementApp with list: $baseTaskList")
+fun BaseManagementApp(modifier: Modifier = Modifier, baseManagementAppViewModel: BaseManagementAppViewModel= viewModel()){
     ZergskiTheme {
         Surface(color = MaterialTheme.colorScheme.surface, modifier = Modifier.fillMaxSize()) {
             Column {
-                NumberOfDrones(dronesHatched.value, {dronesHatched.value++},dronesHatched.value < 5, dronesHatched.value > 0)
-                BaseTaskListView(taskList = baseTaskList, onCheckStateChange = { task, state -> task.onCheckChanged(state)}, onXPressed = {baseTaskList.remove(it)})
+                NumberOfDrones(baseManagementAppViewModel.dronesHatched.value, {baseManagementAppViewModel.spawnDrone()},baseManagementAppViewModel.isAddDroneEnabled, baseManagementAppViewModel.isDroneCountShowing)
+                BaseTaskListView(taskList = baseManagementAppViewModel.baseTaskList, onCheckStateChange = { task, state -> task.onCheckChanged(state)}, onXPressed = {baseManagementAppViewModel.baseTaskList.remove(it)})
             }
         }
     }
 }
 
+class BaseManagementAppViewModel: ViewModel() {
+    val baseTaskList = BaseTaskProvider.getBaseTaskList().toMutableStateList()
+    val dronesHatched = mutableStateOf(0)
+    var isAddDroneEnabled = true
+    var isDroneCountShowing: Boolean = false
+
+    fun spawnDrone() {
+        dronesHatched.value++
+        if(dronesHatched.value > 5) {
+            isAddDroneEnabled = false
+        }
+        if(dronesHatched.value > 0) {
+            isDroneCountShowing = true
+        }
+    }
+
+    fun removeBaseTask(task: BaseTask) {
+        baseTaskList.remove(task)
+    }
+}
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
 @Composable
